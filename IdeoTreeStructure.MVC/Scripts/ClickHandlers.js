03066dd942b32e723b3b501af168aae7ab4a8a11
Ras96 <ras96pl@gmail.com>
2019-01-02 01:20:26 +0100

    Final view checkout

---
+++ IdeoTreeStructure.MVC/Scripts/ClickHandlers.js
@@ -0,0 +1,21 @@
+﻿function removeButtonClicked(clickedId) {
+    $.ajax({
+        url: "RemoveNode",
+        type: 'GET',
+        data: { id: clickedId },
+        success: function () {
+            location.reload();
+        }
+    });
+}
+function hideButtonClicked(clickedId) {
+    parentDiv = $("div#" + clickedId);
+    if (parentDiv.children("div").is(':visible')) {
+        parentDiv.children("div").hide();
+        parentDiv.children("#hideBtn").text("show");
+    }
+    else {
+        parentDiv.children("div").show();
+        parentDiv.children("#hideBtn").text("hide");
+    }
+}
